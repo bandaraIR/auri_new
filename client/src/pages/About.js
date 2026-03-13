0b72d0c6d15342b07b7ff184fc7506df1632d9c9
BandaraIR <imalbandara624@gmail.com>
2026-03-13 07:40:05 +0530

    changed image

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -120,7 +120,7 @@ const About = () => {
                 transition={{ duration: 0.3 }}
               >
                 <img
-                  src="https://images.unsplash.com/photo-1548036328-c9fa89d128fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"
+                  src="https://istanbulfashioncenter.com/cdn/shop/articles/differences-between-boutiques-and-chain--9768_b4b626fe-f827-4612-a212-63dbf64be96f.jpg?v=1761571892"
                   alt="Auri materials"
                 />
               </motion.div>
